2c7755d5d9c2dac0bfcece637febfdd949fd9cb8
Todd Kerpelman <kerp@google.com>
2016-05-27 09:41:01 -0700

    Clean up and copyright notices

--- Planet Tour/GetNewsletterViewController.swift
+++ Planet Tour/GetNewsletterViewController.swift
@@ -1,10 +1,24 @@
-//
-//  GetNewsletterViewController.swift
-//  Planet Tour
-//
-//  Created by Todd Kerpelman on 5/25/16.
-//  Copyright © 2016 Razeware. All rights reserved.
-//
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import UIKit
 
@@ -14,16 +28,17 @@ class GetNewsletterViewController: UIViewController {
   @IBOutlet weak var thankYouLabel: UILabel!
   @IBOutlet weak var submitButton: UIButton!
   @IBOutlet weak var emailTextField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.thankYouLabel.hidden = true
+    thankYouLabel.hidden = true
   }
 
   @IBAction func submitButtonWasPressed(sender: AnyObject) {
     // We won't actually submit an email, but we can pretend
-    self.submitButton.hidden = true
-    self.thankYouLabel.hidden = false
-    self.emailTextField.enabled = false
+    submitButton.hidden = true
+    thankYouLabel.hidden = false
+    emailTextField.enabled = false
   }
 
 }
--- Planet Tour/SolarSystem.swift
+++ Planet Tour/SolarSystem.swift
@@ -1,10 +1,24 @@
-//
-//  SolarSystem.swift
-//  Planet Tour
-//
-//  Created by Todd Kerpelman on 5/23/16.
-//  Copyright © 2016 Razeware. All rights reserved.
-//
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import UIKit
 
@@ -17,7 +31,7 @@ class SolarSystem {
   private init() {
     planets = [
       Planet.init(name: "Mercury", yearInDays: 87.969, massInEarths: 0.3829,
-        funFact: "Trying to find a tactful way of telling the sun it needs some personal space",
+        funFact: "The sun is trying to find a tactful way of telling Mercury it needs some personal space",
         imageName: "Mercury.jpg", imageCredit: "Source: NASA/Johns Hopkins University Applied Physics Laboratory/Carnegie Institution of Washington"),
       Planet.init(name: "Venus", yearInDays: 224.701, massInEarths: 0.815,
         funFact: "Huge fan of saxophone solos in 80s rock songs",
@@ -47,7 +61,7 @@ class SolarSystem {
         imageName: "Neptune.jpg",
         imageCredit: "NASA"),
       Planet.init(name: "Pluto", yearInDays: 90581, massInEarths: 0.002,
-        funFact: "Ostracized by friends for spoiling Game of Thrones before they had a chance to watch.",
+        funFact: "Ostracized by friends for spoiling Game of Thrones before got to watch it.",
         imageName: "Pluto.jpg",
         imageCredit: "NASA/JHUAPL/SwRI")
 
