2415d96afc02b2c7cb4f39c3892cb8d7c6745ec8
Todd Kerpelman <kerp@google.com>
2016-05-31 19:11:09 -0700

    Adding "Waiting" VC

---
+++ Planet Tour/WaitingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WaitingViewController.swift
+//  Planet Tour
+//
+//  Created by Todd Kerpelman on 5/31/16.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class WaitingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
