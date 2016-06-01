edccb086ff200dab6018f0a2fc33d6ee0a62c5ab
Todd Kerpelman <kerp@google.com>
2016-05-31 18:57:24 -0700

    Fetch values remotely!

--- Planet Tour/RCValues.swift
+++ Planet Tour/RCValues.swift
@@ -14,13 +14,33 @@ class RCValues {
 
   private init() {
     loadDefaultValues()
+    fetchCloudValues()
   }
 
   func loadDefaultValues() {
     let appDefaults: [String: NSObject] = [
-      "label_color" : "#FF8888FF"
+      "label_color" : "#FFFFFFFF"
     ]
     FIRRemoteConfig.remoteConfig().setDefaults(appDefaults)
+  }
+
+  func activateDebugMode() {
+    let debugSettings = FIRRemoteConfigSettings.init(developerModeEnabled: true)
+    FIRRemoteConfig.remoteConfig().configSettings = debugSettings!
+  }
+
+  func fetchCloudValues() {
+    // DON'T DO THIS IN PRODUCTION!
+    let fetchDuration : NSTimeInterval = 0
+    activateDebugMode()
+    FIRRemoteConfig.remoteConfig().fetchWithExpirationDuration(fetchDuration) { (status, error) in
+      guard error == nil else {
+        print ("Uh-oh. Got an error fetching remote values \(error)")
+        return
+      }
+      print ("Retrieved values from the cloud!")
+      FIRRemoteConfig.remoteConfig().activateFetched()
+    }
   }
 
   func colorForKey(key: String) -> UIColor {
