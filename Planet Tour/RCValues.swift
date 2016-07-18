021ffb3f78e18ccd3c87a872e59f67b28a02a473
Todd Kerpelman <kerp@google.com>
2016-07-18 17:40:29 +0100

    New copyright notice and default text

--- Planet Tour/RCValues.swift
+++ Planet Tour/RCValues.swift
@@ -1,10 +1,24 @@
-//
-//  RCValues.swift
-//  Planet Tour
-//
-//  Created by Todd Kerpelman on 5/31/16.
-//  Copyright © 2016 Razeware. All rights reserved.
-//
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import Foundation
 import Firebase
@@ -47,8 +61,8 @@ class RCValues {
       ValueKey.navTintColor.rawValue: "#FBB03B",
       ValueKey.detailTitleColor.rawValue: "#FFFFFF",
       ValueKey.detailInfoColor.rawValue: "#CCCCCC",
-      ValueKey.subscribeBannerText.rawValue: "Liking Planet Tour? Get our newsletter!",
-      ValueKey.subscribeBannerButton.rawValue: ">>",
+      ValueKey.subscribeBannerText.rawValue: "Like Planet Tour?",
+      ValueKey.subscribeBannerButton.rawValue: "Get our newsletter!",
       ValueKey.subscribeVCText.rawValue: "Want more astronomy facts? Sign up for our newsletter!",
       ValueKey.subscribeVCButton.rawValue: "Subscribe",
       ValueKey.shouldWeIncludePluto.rawValue: false,
